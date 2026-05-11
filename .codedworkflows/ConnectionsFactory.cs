751cbcc952e8587aaa25a25d1b73027f63596092
KhoaPham <khoadang@quintet.co.kr>
2026-05-11 19:36:44 +0900

    feat: CheckAvailable - ExportJsonToExcel, SendMail, SendMailCheckAvailable

--- .codedworkflows/ConnectionsFactory.cs
+++ .codedworkflows/ConnectionsFactory.cs
@@ -12,11 +12,8 @@ namespace REF_RPA
 
     public class DriveFactory
     {
-        public UiPath.GSuite.Activities.Api.DriveConnection RPA_thanhtuyen_quintet_co_kr { get; set; }
-
         public DriveFactory(ICodedWorkflowsServiceContainer resolver)
         {
-            RPA_thanhtuyen_quintet_co_kr = new UiPath.GSuite.Activities.Api.DriveConnection("f01b3f94-ea99-4205-b50e-1c8205d72612", resolver);
         }
     }
 
@@ -29,11 +26,11 @@ namespace REF_RPA
 
     public class GmailFactory
     {
-        public UiPath.GSuite.Activities.Api.GmailConnection RPA_thanhtuyen_quintet_co_kr { get; set; }
+        public UiPath.GSuite.Activities.Api.GmailConnection My_Workspace_20145393_student_hcmute_edu_vn { get; set; }
 
         public GmailFactory(ICodedWorkflowsServiceContainer resolver)
         {
-            RPA_thanhtuyen_quintet_co_kr = new UiPath.GSuite.Activities.Api.GmailConnection("a4da268b-7fc3-4a63-b8ec-3028fc01713c", resolver);
+            My_Workspace_20145393_student_hcmute_edu_vn = new UiPath.GSuite.Activities.Api.GmailConnection("c366934d-7fd1-4973-b886-a4fad4f6113e", resolver);
         }
     }
 
